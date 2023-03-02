137082121a1f3dfe52defff0013a0e90f040f01e
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-03-02 15:38:19 -0300

    mapped image icons

---
+++ src/assets/images.js
@@ -0,0 +1,12 @@
+import turningArrow from "./seta_virar.png"
+import playArrow from "./seta_play.png"
+import rightIcon from "./icone_certo.png"
+import almostIcon from "./icone_quase.png"
+import wrongIcon from "./icone_erro.png"
+export const mappedImgs = new Map([
+    ["turningArrow", turningArrow],
+    ["playArrow", playArrow],
+    ["rightIcon", rightIcon],
+    ["almostIcon", almostIcon],
+    ["wrongIcon", wrongIcon]]);
+    
