38521ef011ea5a78ae877a8decea3097e5429e24
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-03-02 15:34:21 -0300

    removed unnecessary lines

--- src/components/InitialPage.jsx
+++ src/components/InitialPage.jsx
@@ -1,7 +1,5 @@
 import { InitialPageButton, InitialPageContainer, LogoImg, LogoText } from "./Shared/InitialPageItems";
 import logo from "../assets/logo.png";
-
-
 export function InitialPage({changePage}) {
   return (<InitialPageContainer>
     <LogoImg src={logo} />
